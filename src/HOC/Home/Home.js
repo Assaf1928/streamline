95a1ac5d043263c71fe51eca1c9f72bd6207705f
assaf <assaf1928@gmail.com>
2022-12-13 20:13:05 +0200

    facelift

---
+++ src/HOC/Home/Home.js
@@ -0,0 +1,19 @@
+import './Home.css';
+import { FaPlusCircle  } from 'react-icons/fa';
+import StreamLineLogo from'./images/stream_line.png';
+
+function Home() {
+  return (
+    <div className="App">
+      <div className='stream_line_container'>
+        <div  className='x'>
+      <img src={StreamLineLogo} className="stream_line_logo"/>
+      </div>
+      </div>
+     <div className="plus_circle_container"> <div><FaPlusCircle /></div>
+     <div>Add New Sample</div></div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/HOC/SampleDetails/SampleDetails.js
@@ -0,0 +1,43 @@
+import './SampleDetails.css';
+import Map from '../../LOC/Map/Map.js'
+
+function SampleDetails() {
+  return (
+    <div className="App">
+      <div className='select_container'>
+      <div>Sample Details:</div>
+      <select className="spot_select">
+        <option>Choose spot</option>
+        <option></option>
+      </select>
+      </div>
+      <div className='inputs'>
+      <div className='input_container'>
+        <div> 
+        <input placeholder='Sample Id'/>
+        </div>
+        <div> 
+        <input placeholder='Sample Id'/>
+        </div>
+        <div> 
+        <input placeholder='Sample Id'/>
+        </div>
+        <div> 
+        <input placeholder='Sample Id'/>
+        </div>
+      </div>
+      </div>
+      <Map/>
+      <div className='btn_container'>
+      <button className='save_button'>Save</button>
+      </div>
+    </div>
+  );
+}
+
+export default SampleDetails;
+
+
+
+
+
