a79294500d1abef8074c568f06b9927a5a1dea11
assaf <assaf1928@gmail.com>
2022-12-14 19:23:29 +0200

    router added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Home from './HOC/Home/Home'
 import SampleDetails from './HOC/SampleDetails/SampleDetails';
 import SampleQR from './HOC/SampleQR/SampleQR';
 import SampleDetailsSum from './HOC/SampleDetailsSum/SampleDetailsSum';
 import SamplePrintQR from './HOC/SamplePrintQR/SamplePrintQR';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <SamplePrintQR />
+    <Router>
+    <Routes>
+          <Route exact path='/' element={< Home />}></Route>
+          <Route exact path='/SampleDetails' element={< SampleDetails />}></Route>
+          <Route exact path='/SampleQR' element={< SampleQR />}></Route>
+          <Route exact path='/SampleDetailsSum' element={< SampleDetailsSum />}></Route>
+          <Route exact path='/SamplePrintQR' element={< SamplePrintQR />}></Route>
+   </Routes>
+</Router>
 );
 
-reportWebVitals();
