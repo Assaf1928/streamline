6bd31a4981300160a476073823fd0443975dbae4
assaf <assaf1928@gmail.com>
2023-01-01 21:15:30 +0200

    fix-overview

--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import  { Component }  from 'react';
 import { Chart } from "react-google-charts";
-import {TubeTypes} from '../Consts/TubesTypes'
+import {firestore} from "../firebase.js"
+import moment from 'moment'
+import {getDocs, collection} from "@firebase/firestore"
+
 class Overview extends Component {
     constructor(props) {
       super(props);
       this.handleChange = this.handleChange.bind(this);
       this.state = {
         data: [
-            ["Date","Value"],
-            [new Date().toISOString().substring(0,10), 10],
-            [new Date().toISOString().substring(0,10), 20],
-            [new Date().toISOString().substring(0,10), 30],
-            [new Date().toISOString().substring(0,10), 40],
+
           ],
           options: {
             title: "Value For Date",
@@ -22,7 +21,55 @@ class Overview extends Component {
       };
     }
   
-    componentDidMount() {
+    async componentDidMount() {
+
+      let newArray = [];
+      newArray.push(["Date","Value"])
+      const ref =  collection(firestore,"samples");
+      let querySnapshot = await getDocs(ref)
+      let newSamplesArray = []
+      querySnapshot.forEach((doc) => {
+        newSamplesArray.push(doc.data())
+      })
+      var startFrom = new Date();
+      for(let i = 0; i < 4; i++) {
+        var date = new Date();
+        date.setDate(startFrom.getDate()+i);
+        let indexCount = 0;
+        let count = 0;
+        newSamplesArray.forEach(sample => {
+          date.setHours(0,0,0,0)
+          let sampleDate =new Date(sample.time)
+          sampleDate.setHours(0,0,0,0)
+       //   console.log('sampleDate' + sampleDate, 'date' + date)
+         // console.log(sampleDate.getTime() == date.getTime())
+
+          if(sampleDate.getTime() == date.getTime()) {
+            if(sample.tubes) {
+            let BOD = 1 
+            let bodTubes =   sample.tubes.filter(t=>t.type == BOD)
+            bodTubes.forEach(m=> {
+              if(m.value) {
+                count += m.value
+                indexCount++;
+              }
+            })
+            }
+          }
+        });
+        let strDate = date.toLocaleDateString('pt-br').split( '/' ).reverse( ).join( '-' )
+        console.log(date)
+        console.log(strDate)
+        newArray.push([strDate,count / indexCount])
+        
+      }
+      this.setState({data: newArray})
+
+      // 14 Date 
+
+      // sort by date, per each date calculate average by tube typeId
+      // value
+      //this.setState({})
     }
   
     componentWillUnmount() {
@@ -31,9 +78,6 @@ class Overview extends Component {
   
     handleChange() {
       // Update component state whenever the data source changes
-    }
-    getPropName(t) {
-     return Object.getOwnPropertyNames(t)
     }
       
     render() {
